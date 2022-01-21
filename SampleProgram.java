92d9dd82acfd9d614da41d2fd4430a3b362d13a1
Dhamodharan-07 <98150827+Dhamodharan-07@users.noreply.github.com>
2022-01-21 13:54:07 +0530

    Add files via upload

---
+++ SampleProgram.java
@@ -0,0 +1,20 @@
+package Basicsprogram;
+
+
+
+
+public class SampleProgram {
+   public static void main(String[]args)
+{
+	//print hello world
+	   System.out.println("Hello world,good morning");
+	   System.out.println("i am gaining java knownledge");
+
+int num=10;//we have initialised 10 in num variable at compile time
+
+System.out.println("the vale is="+num);//the vale is=10+is a concatinate operator
+}
+   
+}
+	
+		
